18bcad50cc5785374d588cc730f54ae4bb35c6f6
Luis <mluise2406@gmail.com>
2020-08-29 09:48:29 -0600

    Multer added

--- backend/config/storage.js
+++ backend/config/storage.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const multer = require('multer');
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) =>{
-    cb(null, path.join(__dirname, '../public/uploads/img'));
-  },
-  filename: (req,file,cb)=> {
-    cb(null, Date.now() + path.extname(file.originalname));
+const storage = multer.diskStorage(
+  {
+    destination: (req, file, cb) =>{
+      cb(null, path.join(__dirname, '../public/uploads/img'));
+    },
+    filename: (req,file,cb)=> {
+      cb(null, Date.now() + path.extname(file.originalname));
+    }
   }
-});
+);
 
 module.exports = storage;
